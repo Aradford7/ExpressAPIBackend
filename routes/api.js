37d7754783487841341b80bcef44166329787faa
Ashley Radford <aradford7@outlook.com>
2019-05-13 11:36:48 -0700

    temp

---
+++ routes/api.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  return res.json({data: 'Received a GET HTTP method'});
+});
+
+router.post('/', (req, res) => {
+  return res.json({
+    body: req.body
+  });
+});
+
+router.put('/', (req, res) => {
+  return res.json({data: 'Received a PUT HTTP method'});
+});
+
+router.delete('/', (req, res) => {
+  return res.json({data: 'Received a DELETE HTTP method'});
+});
+
+module.exports = router;
