8ecb0354d112c67f56d23da3b2edcb3dbd2d71ca
Ashley Radford <aradford7@outlook.com>
2019-05-13 14:47:58 -0700

    asd

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  return res.json({data: 'Received a GET HTTP method'});
+//get MarvelAPI from models
+const Marvel = require('../models/marvel');
+
+router.get('/', (req, res, next) => {
+  console.log(results, 'this is get all Marvel Characters')
+  try {
+    const results = Marvel.find(data.results);
+    // doGetComics =  async (e) => {
+    //   e.preventDefault();
+    //   const results = await Marvel.find();
+    //response to react
+    res.json({
+      status: 200,
+      data: results
+    });
+  }catch(err){
+    res.send(err)
+  };
 });
+
 
 router.post('/', (req, res) => {
   return res.json({
