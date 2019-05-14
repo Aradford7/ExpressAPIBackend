249fe694a1a48833c08684828ce85587bf5fa84c
Ashley Radford <aradford7@outlook.com>
2019-05-14 10:59:25 -0700

    list

--- routes/marvelapi.js
+++ routes/marvelapi.js
@@ -6,12 +6,21 @@ const fetch = require('node-fetch');
 
 //GET ROUTE
 //All characters
-router.get('/',  (req, res, next) => {
+router.get("/character/:id", (req,res)=>{
   try {
-    fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=thesoer&apikey=c4bed87b70142a7ee8645e5466f98334&hash=f65084c44934b7db642943416b484ac9`)
+    fetch(`https://gateway.marvel.com:443/v1/public/characters/${req.params.id}/comics?ts=thesoer&apikey=c4bed87b70142a7ee8645e5466f98334&hash=f65084c44934b7db642943416b484ac9`)
+    .then(res => res.json())
+    .then(json => res.json({comics: json.data.results, success: true}))
+  }catch(err){
+    res.send(err)
+  };
+})
+
+router.get('/:name',  (req, res, next) => {
+  try {
+    fetch(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${req.params.name}&ts=thesoer&apikey=c4bed87b70142a7ee8645e5466f98334&hash=f65084c44934b7db642943416b484ac9`)
     .then(res => res.json())
     .then(json => res.json({ char: json.data.results }))
-
   }catch(err){
     res.send(err)
   };
